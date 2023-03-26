adedd473a330aa5ba67e92b678fb9ebc5eadc37c
NAG-LG <nagindra.cs@gmail.com>
2023-03-26 08:47:21 +0530

    webserver: Adding WEBSERVER
    Issues exist, local code pushed.

--- Boost-test.cpp
+++ Boost-test.cpp
@@ -2,17 +2,17 @@
 #include <boost/array.hpp>
 #include <iostream>
 
-using namespace std;
-int main()
-{
-    boost::array<int, 10> arr
-        = { { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 } };
-    for (int i = 0; i < 10; i++) {
-        cout << "Geek Rank is :" << arr[i] << "*"
-            << "\n";
-    }
-
-    getchar();
-
-    return 0;
-}
+//using namespace std;
+//int main()
+//{
+//    boost::array<int, 10> arr
+//        = { { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 } };
+//    for (int i = 0; i < 10; i++) {
+//        cout << "geek rank is :" << arr[i] << "*"
+//            << "\n";
+//    }
+//
+//    getchar();
+//
+//    return 0;
+//}
